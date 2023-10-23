164d53c16eeefa9b3fd47eb2e33af57e7333afa0
vitaliy-101 <70775970+vitaliy-101@users.noreply.github.com>
2023-10-23 16:04:43 +0300

    Create dotaInformation

--- Parser.py
+++ Parser.py
@@ -4,6 +4,14 @@ from Hero import Hero
 
 class ParserFile(object):
     heroes = list()
+    dotaInformation = ("Dota 2 — многопользовательская командная компьютерная игра в жанре MOBA, разработанная и "
+                       "изданная корпорацией Valve. Игра является продолжением DotA — пользовательской "
+                       "карты-модификации для игры Warcraft III: Reign of Chaos и дополнения к ней Warcraft III: The "
+                       "Frozen Throne. Игра изображает сражение на карте особого вида; в каждом матче участвуют две "
+                       "команды по пять игроков, управляющих разными «героями» — персонажами с различными наборами "
+                       "способностей и характеристиками. Для победы в матче команда должна уничтожить особый объект — "
+                       "«крепость», принадлежащий вражеской стороне, и защитить от уничтожения собственную "
+                       "«крепость». Dota 2 работает по модели free-to-play с элементами микроплатежей.")
     def parse(self):
         URL = "https://dota2.ru/"
         req = requests.get(URL + "heroes/")
@@ -23,3 +31,7 @@ class ParserFile(object):
     @property
     def getHeroes(self):
         return self.heroes
+
+    @property
+    def getDotaInformation(self):
+        return self.dotaInformation
